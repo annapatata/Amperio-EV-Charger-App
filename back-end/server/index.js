774d11dc07c4f6b1cef42f00b34532f95f863b1f
ntua-el22097 <el22097@mail.ntua.gr>
2025-12-15 23:53:06 +0200

    added https and updated port for API

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const https = require('https'); 
+const fs = require('fs');       // insert 'FileSystem'
+
+//import routes
 const userRoutes = require('./routes/userRoutes'); 
 const authRoutes = require('./routes/authRoutes'); 
 
@@ -12,6 +16,13 @@ app.use(express.json()); // JSON format data
 app.use('/api/users', userRoutes); 
 app.use('/api/auth', authRoutes); 
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+const sslOptions = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+};
+
+const PORT = 9876;
+
+https.createServer(sslOptions, app).listen(PORT, () => {
+  console.log(`Secure Server is running on https://localhost:${PORT}`);
 });
