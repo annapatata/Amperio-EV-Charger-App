4de7f1e542e3f2db4f86d4e2f95105227690c3ba
ntua-el22097 <el22097@mail.ntua.gr>
2025-12-16 12:22:35 +0200

    error handler middleware and datetime formatting

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -3,19 +3,35 @@ const cors = require('cors');
 const https = require('https'); 
 const fs = require('fs');       // insert 'FileSystem'
 
+//import error-handling middleware
+const errorHandler = require('./middleware/errorHandler');
+
 //import routes
 const userRoutes = require('./routes/userRoutes'); 
 const authRoutes = require('./routes/authRoutes'); 
+//const adminRoutes = require('./routes/adminRoutes'); 
 
 const app = express();
 
 app.use(cors()); // React-server comm
 app.use(express.json()); // JSON format data
+app.use(express.urlencoded({ extended: true })); // data parsing
 
 // mount Routes
 app.use('/api/users', userRoutes); 
 app.use('/api/auth', authRoutes); 
+//app.use('/api/admin', adminRoutes);
 
+// error 404 (Not Found) 
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    res.status(404);
+    next(error);
+});
+
+// --- GLOBAL ERROR HANDLER ---
+// Must be the last middleware used
+app.use(errorHandler);
 const sslOptions = {
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.cert')
